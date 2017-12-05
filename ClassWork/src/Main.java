ffda12c0b0beb57cfb20832cbbe774e74d11f0dd
Павел <pavel@MacBook-Pro-Pavel.local>
2017-12-05 15:35:07 +0300

    v1.0 with GUE

--- ClassWork/src/Main.java
+++
@@ -1,6 +0,0 @@
-/**
- * Project name: ClassWork
- * Created by pavel on 04.09.17.
- */
-public class Main {
-}
--- JavaRush/JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task05/task0529/ytf.java
+++
@@ -1,7 +0,0 @@
-package com.javarush.task.task05.task0529;
-
-/**
- * Created by pavel on 01.08.17.
- */
-public class ytf {
-}
